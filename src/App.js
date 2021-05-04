d5bb480d30097c8e733247bd9facc3505ea744de
Makha Blu Wakpa <bluwakpa@Makhas-MacBook-Pro.local>
2021-05-03 20:22:53 -0700

    last save switch

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { useState, useContext } from 'react';
 import data from './data'
 import ApiContext from './ApiContext';
-import { useParams } from 'react-router-dom'
+// import { useParams } from 'react-router-dom'
 // import { v4 as uuidv4 } from 'uuid';
-// import CategoryNav from './CategoryNav'
-// import TaskList from './TaskList'
-// import Task from './Task'
-// import FilteredTasks from './FilteredTasks'
+
 
 export default function App() {
   // console.log('props', props)
   // {}
   const context = useContext(ApiContext)
   console.log('context.data', context)
+
   // {
   //   categories: [],
   //   tasks: [],
@@ -21,25 +19,31 @@ export default function App() {
   //   editCategory: () => {},
   //   editTask: () => {},
   // }
-  // const [categories, setCategories] = useState(data.categories)
+
   const [tasks, setTasks] = useState(data.tasks)
   console.log('tasks', tasks)
+
   // "tasks": [{tasks}]
-  console.log('tasks.content', tasks.content)
+
+  console.log('tasks.content', tasks[0].content)
   // undef
-  
-  const task = context.tasks.find(task => task.id === data.match.params.id) || {};
-  console.log('task', task)
+
+  // wrap this inside a function and pass in a current htmlId
+  // const task = context.tasks.find(task => task.id === data) || {};
+  // console.log('task', task)
   // {}
-  const taskIndex = context.tasks.indexOf(task)
-  console.log('taskIndex', taskIndex)
+
+  // indexOf may not work with objects, might need library underscore or lodash
+  // const taskIndex = context.tasks.indexOf(task)
+  // console.log('taskIndex', taskIndex)
   //  -1
+
+  // argue a single task.content based on .index
   const [content, setContent] = useState(tasks.content)
   console.log('content', content)
   // undef
-  // const content = context.data.tasks.content
-  // console.log(content)
 
+  // consider push, add to end, may not need splice
   function createTask(e, i) {
     const newTasks = [...tasks];
     newTasks.splice(i + 1, 0, {
@@ -51,6 +55,21 @@ export default function App() {
       document.forms[0].elements[i + i].focus();
     }, 0);
   }
+
+  // setTasks([
+  //   ...tasks,
+  //   {
+  //     id: uuidv4(),
+  //     task,
+  //     complete:false
+  //   }
+  // ])
+
+  // const setStatusTask = (id, status) => {
+  //   setTasks(tasks.map(tast => task.id === id ? {...task, complete: status} : task))
+  // }
+
+  // const { addTask, deleteTask } = useTaskState ([]);
 
   function updateTask(e, i) {
     const newTasks = [...tasks];
@@ -84,6 +103,13 @@ export default function App() {
     }
   }
 
+  const handleClickDelete = (e) => {
+    e.preventDefault()
+    const task = data.tasks.filter(task => task.id === data) || {};
+    let deleted = context.tasks.filter(task => task.id !== data)
+    console.log('data.tasks', data.tasks)
+  }
+
 
   return (
     <div className="app">
@@ -92,8 +118,16 @@ export default function App() {
       </div>
       <form className="task-list">
         <ul>
+          <div>
+            {/* Text box defaults as students information based on id */}
+            <input 
+              placeholder="Add task"
+              type="text"
+            />
+            <button>Add</button>
+          </div>
           {tasks.map((task, i) => (
-            <div className={`task ${task.complete && 'task-is-ed'}`}>
+            <div className={`task ${task.complete && 'task-is-complete'}`}>
               <div className={'checkbox'} onClick={() => toggleTaskComplete(i)}>
                 {task.complete && (
                   <span>&#x2714;</span>
@@ -101,10 +135,15 @@ export default function App() {
               </div>
               <input
                 type="text"
-                value={tasks.content}
+                value={task.content}
                 onEnter={e => handleEnter(e, i)}
                 onChange={e => updateTask(e, i)}
               />
+              <button
+                className="Task_delete"
+                type="button"
+                onClick={handleClickDelete}
+              >Delete</button>
             </div>
           ))}
         </ul>
--- src/data.js
+++ src/data.js
@@ -1,54 +1,19 @@
 export default {
-  "categories": [
-    {
-      "id": "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1",
-      "name": "Personal"
-    },
-    {
-      "id": "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1",
-      "name": "Work"
-    },
-    {
-      "id": "b07162f0-ffaf-11e8-8eb2-f2801f1b9fd1",
-      "name": "Chores"
-    }
-  ],
   "tasks": [
     {
       "id": "cbc787a0-ffaf-11e8-8eb2-f2801f1b9fd1",
       "content": "Excercise",
-      "complete": false,
-      "categoryId": "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1",
+      "complete": true,
     },
     {
       "id": "d26e0034-ffaf-11e8-8eb2-f2801f1b9fd1",
       "content": "Debug React app",
       "complete": false,
-      "categoryId": "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1",
-    },
-    {
-      "id": "d26e01a6-ffaf-11e8-8eb2-f2801f1b9fd1",
-      "content": "Attend scrum",
-      "complete": false,
-      "categoryId": "b07161a6-ffaf-11e8-8eb2-f2801f1b9fd1",
     },
     {
       "id": "d26e0570-ffaf-11e8-8eb2-f2801f1b9fd1",
       "content": "Meditate",
       "complete": false,
-      "categoryId": "b0715efe-ffaf-11e8-8eb2-f2801f1b9fd1",
-    },
-    {
-      "id": "d26e0980-ffaf-11e8-8eb2-f2801f1b9fd1",
-      "content": "Mail package",
-      "complete": false,
-      "categoryId": "b07162f0-ffaf-11e8-8eb2-f2801f1b9fd1",
-    },
-    {
-      "id": "d26e0aac-ffaf-11e8-8eb2-f2801f1b9fd1",
-      "content": "Make the bed",
-      "complete": false,
-      "categoryId": "b07162f0-ffaf-11e8-8eb2-f2801f1b9fd1",
     },
   ]
 }
