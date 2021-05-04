aee63b01dd23573dc6e6c3f0e82b0c741ec9fdd6
Makha Blu Wakpa <bluwakpa@Makhas-MacBook-Pro.local>
2021-05-03 21:54:26 -0700

    TaskListMain

---
+++ src/TaskListMain.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Task from './Task'
+import CircleButton from '../CircleButton/CircleButton'
+import './TaskListMain.css'
+
+export default function TaskListMain(props) {
+    return (
+        <section className='TaskListMain'>
+            <ul>
+                {props.tasks.map(task =>
+                    <li key={task.id}>
+                        <Task
+                            id={task.id}
+                            name={task.name}
+                            modified={task.modified}
+                        />
+                    </li>
+                )}
+            </ul>
+            <div className='TaskListMain__button-container'>
+                <CircleButton
+                    tag={Link}
+                    to='/add-task'
+                    type='button'
+                    className='TaskListMain__add-task-button'
+                >
+                    <FontAwesomeIcon icon='plus' />
+                    <br />
+          Task
+        </CircleButton>
+            </div>
+        </section>
+    )
+}
+
+TaskListMain.defaultProps = {
+    tasks: [],
+}
