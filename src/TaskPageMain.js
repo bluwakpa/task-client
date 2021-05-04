f4e18db4a951cdaba4ccb6f60d2988b567979f41
Makha Blu Wakpa <bluwakpa@Makhas-MacBook-Pro.local>
2021-05-03 21:57:22 -0700

    TaskPageMain

---
+++ src/TaskPageMain.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Task from './Task'
+import './index.css'
+
+export default function TaskPageMain(props) {
+  return (
+    <section className='TaskPageMain'>
+      <Task
+        id={props.task.id}
+        name={props.task.name}
+        modified={props.task.modified}
+      />
+      <div className='TaskPageMain__content'>
+        {props.task.content.split(/\n \r|\n/).map((para, i) =>
+          <p key={i}>{para}</p>
+        )}
+      </div>
+    </section>
+  )
+}
+
+TaskPageMain.defaultProps = {
+    task: {
+    content: '',
+  }
+}
