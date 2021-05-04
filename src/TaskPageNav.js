5249ae335b0f83849b80946397b6557e08c1c9ec
Makha Blu Wakpa <bluwakpa@Makhas-MacBook-Pro.local>
2021-05-03 21:50:31 -0700

    TaskPageNav

---
+++ src/TaskPageNav.js
@@ -0,0 +1,32 @@
+import React from 'react'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import CircleButton from '../CircleButton/CircleButton'
+import './index.css'
+
+export default function TaskPageNav(props) {
+  return (
+    <div className='TaskPageNav'>
+      <CircleButton
+        tag='button'
+        role='link'
+        onClick={() => props.history.goBack()}
+        className='TaskPageNav__back-button'
+      >
+        <FontAwesomeIcon icon='chevron-left' />
+        <br />
+        Back
+      </CircleButton>
+      {props.category && (
+        <h3 className='TaskPageNav__category-name'>
+          {props.category.name}
+        </h3>
+      )}
+    </div>
+  )
+}
+
+TaskPageNav.defaultProps = {
+  history: {
+    goBack: () => {}
+  }
+}
